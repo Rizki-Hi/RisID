7caa2f740069099de2a27fa1db97211bf88162c8
Rizki-Hi <74732749+Rizki-Hi@users.noreply.github.com>
2020-11-22 10:18:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -125,8 +125,8 @@ conn.sendMessage(id, teks, MessageType.text)
 
 if (text.includes("#nulis")){
   const teks = text.replace(/#nulis /, "")
-axios.get(`https://st4rz.herokuapp.com/api/nulis?text=${teks}`).then((res) => {
-    let hasil = `Download sendiri ya hasilnya dibawah, soalnya kalo dikirim langsung hasilnya blur\n\n${res.data.result.data}`;
+axios.get(`https://mhankbarbar.herokuapp.com/nulis?text=${teks}&apiKey=zFuV88pxcIiCWuYlwg57`).then((res) => {
+    let hasil = `Download sendiri ya hasilnya dibawah, soalnya kalo dikirim langsung hasilnya blur\n\n${res.data.result}`;
     conn.sendMessage(id, hasil ,MessageType.text);
 })
 }
